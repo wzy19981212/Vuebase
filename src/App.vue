ffe3f8dad8e00cb1b3b4d8e51db003b0b5824630
王舟旸 <1045300366@qq.com>
2021-11-19 16:12:36 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <mubutton></mubutton>
+  </div>
+</template>
+
+<script>
+import mubutton from "./components/mubutton.vue";
+export default {
+  components: {
+    mubutton,
+  },
+};
+</script>
+
+<style lang="less" scoped>
+</style>
---
+++ src/components/mubutton.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>我爱刘亦菲</h1>
+    <button @click="count++">爱了{{ count }}次</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped>
+</style>
